e488b203601e3af632fb52d1e2df67e13b6dd360
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2014-09-19 11:49:12 -0700

    cards completed. not refreshing

--- app/assets/javascripts/views/board_show.js
+++ app/assets/javascripts/views/board_show.js
@@ -15,14 +15,62 @@ TrelloClone.Views.BoardShow = Backbone.View.extend({
     "submit .form-new-list": "create",
     "click button.btn-delete-list": "destroy",
     "click button.btn-cancel": "cancelForm",
+
     "click button.btn-delete-card": "destroyCard",
     "click button.btn-create-card": "newCardForm",
     "submit .form-new-card": "createCard"
   },
 
-  newCardForm: function() {
+  createCard: function(event) {
     event.preventDefault();
+    var $form = $(event.currentTarget);
+    var that = this;
+    var listId = $form.parent().data("list-id");
+    var list = this.model.lists().get(listId);
+    var newCard = new TrelloClone.Models.Card({
+      title: $form.find(".new-card-title").val(),
+      ord: $form.find(".new-card-order").val(),
+      list_id: listId
+    });
 
+    newCard.save({}, {
+      success: function() {
+        $(list).add(newCard);
+        that.removeCardForm($form, listId);
+      }
+    });
+  },
+
+  create: function(event) {
+    event.preventDefault();
+    var $form = $(event.currentTarget);
+    var that = this;
+    var newList = new TrelloClone.Models.List({
+      title: $form.find(".new-list-title").val(),
+      ord: $form.find(".new-list-order").val(),
+      board_id: that.model.id
+    });
+
+    newList.save({}, {
+      success: function() {
+        that.model.lists().add(newList);
+        that.removeForm($form);
+      }
+    });
+  },
+
+  removeCardForm: function($form, listId) {
+    $form.remove();
+    var $newCardButton = $("<button class='btn-create-card' data-list-id='" + listId + "'>")
+    $newCardButton.text("add card");
+    $("li#new-card-form-" + listId).append($newCardButton);
+  },
+
+  newCardForm: function(event) {
+    var listId = $(event.currentTarget).data("list-id");
+    var $form = $(this.$el.find("li#new-card-form-" + listId));
+    $form.find(".btn-create-card");
+    $form.html(this.newCardTemplate());
   },
 
   cancelForm: function(event) {
@@ -42,24 +90,6 @@ TrelloClone.Views.BoardShow = Backbone.View.extend({
     var $newListButton = $("<button class='btn-create-list'>");
     $newListButton.text("Create");
     $("li.new-list-form").append($newListButton);
-  },
-
-  create: function(event) {
-    event.preventDefault();
-    var $form = $(event.currentTarget);
-    var that = this;
-    var newList = new TrelloClone.Models.List({
-      title: $form.find(".new-list-title").val(),
-      ord: $form.find(".new-list-order").val(),
-      board_id: that.model.id
-    });
-
-    newList.save({}, {
-      success: function() {
-        that.model.lists().add(newList);
-        that.removeForm($form);
-      }
-    });
   },
 
   destroyCard: function(event) {
