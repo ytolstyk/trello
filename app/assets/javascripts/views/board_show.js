07ec88f0e97f29b02db334e529cefa2e9aab2aa9
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2014-09-19 11:01:24 -0700

    before creating cards

--- app/assets/javascripts/views/board_show.js
+++ app/assets/javascripts/views/board_show.js
@@ -1,7 +1,9 @@
 TrelloClone.Views.BoardShow = Backbone.View.extend({
 
   template: JST['boards/show'],
-  formTemplate: JST['lists/new'],
+  newListTemplate: JST['lists/new'],
+  newCardTemplate: JST['cards/new'],
+
 
   initialize: function() {
     this.listenTo(this.model, "sync", this.render);
@@ -12,10 +14,18 @@ TrelloClone.Views.BoardShow = Backbone.View.extend({
     "click button.btn-create-list": "newForm",
     "submit .form-new-list": "create",
     "click button.btn-delete-list": "destroy",
-    "click button.btn-cancel": "cancelForm"
+    "click button.btn-cancel": "cancelForm",
+    "click button.btn-delete-card": "destroyCard",
+    "click button.btn-create-card": "newCardForm",
+    "submit .form-new-card": "createCard"
   },
 
-   cancelForm: function(event) {
+  newCardForm: function() {
+    event.preventDefault();
+
+  },
+
+  cancelForm: function(event) {
     event.preventDefault();
     var $form = $(this.$el.find(".form-new-list"));
     this.removeForm($form);
@@ -24,7 +34,7 @@ TrelloClone.Views.BoardShow = Backbone.View.extend({
   newForm: function() {
     var $form = $(this.$el.find("li.new-list-form"));
     $form.find(".btn-create-list");
-    $form.html(this.formTemplate());
+    $form.html(this.newListTemplate());
   },
 
   removeForm: function($form) {
@@ -49,6 +59,17 @@ TrelloClone.Views.BoardShow = Backbone.View.extend({
         that.model.lists().add(newList);
         that.removeForm($form);
       }
+    });
+  },
+
+  destroyCard: function(event) {
+    event.preventDefault();
+    var id = $(event.currentTarget).data("id");
+    var listId = $(event.currentTarget).data("list-id");
+    var card = this.model.lists().get(listId).cards().get(id);
+    var list = this.model.lists().get(listId);
+    card.destroy({
+      success: list.fetch()
     });
   },
 
